9eeaf48e2c5b5f9327a99637a389b838dd4ee81b
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-23 14:24:55 +0000

    teste

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setError(null);
 
     try {
-      const res = await fetch("https://express-backend-example2.vercel.app/api/login", {
+      const res = await fetch("https://express-backend-example2.vercel.app/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
